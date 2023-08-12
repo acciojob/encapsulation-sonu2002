5f81122fd9fb05547e8c5c878b2f6a9ecadcc5f6
sonu <sonuphogat0015@gmail.com>
2023-08-12 11:10:50 +0530

    assignment 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+
+     public static void main(String[] args) {
+          RWOnly rwOnlyObj = new RWOnly("John Doe");
+
+//          rwOnlyObj.name = "Jane Smith"; // This will result in a compilation error
+//          name has private access in com.driver.RWOnly
+
+
+          // Try printing the name variable
+//          System.out.println("Name: " + rwOnlyObj.name);
+
+          rwOnlyObj.setName("Sonu");
+          System.out.println(rwOnlyObj.getName());
+
+     }
+
+
   
 }
